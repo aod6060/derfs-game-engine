f141055a073bd8af4d96c418cd4c0fbd9151c106
aod6060 <a.o.d.6060@gmail.com>
2026-02-07 19:08:28 -0600

    #16 added in Entity::getGlobalPosition()

--- src/engine/src/manager/entity.cpp
+++ src/engine/src/manager/entity.cpp
@@ -229,11 +229,21 @@ namespace manager {
     }
 
     glm::mat4 Entity::toParentMatrix(Entity* entity) {
+        if(entity == nullptr) {
+            return glm::mat4(1.0f);
+        }
+
         glm::mat4 m = entity->transform.toModel();
         if(entity->hasParent()) {
             m = this->toParentMatrix(entity->parent) * m;
         }
         return m;
+    }
+
+    glm::vec3 Entity::getGlobalPosition() {
+        glm::vec4 position = glm::vec4(this->transform.position, 1.0f);
+        position = this->toParentMatrix(this->parent) * position;
+        return glm::vec3(position.x, position.y, position.z);
     }
 
     void Entity::addChildEntity(Entity* entity) {
--- src/engine/src/sys.hpp
+++ src/engine/src/sys.hpp
@@ -770,10 +770,17 @@ namespace manager {
         void load(Json::Value value);
 
         bool hasParent();
+        
+        // @decrepracated
         glm::mat4 calculateParent();
+        
+        // @decrepracated
         glm::mat4 calculateParentMatrix();
+
         glm::mat4 toParentMatrix(Entity* entity);
 
+        glm::vec3 getGlobalPosition();
+        
         void addChildEntity(Entity* entity);
         void removeEntity(Entity* entity);
     };
@@ -887,7 +894,7 @@ namespace script {
     // manager
     void manager_load_library(lua_State* l);
     int manager_createEntityFromPrefab(lua_State* l);
-    
+
     // behavior
     void manager_behavior_load_library(lua_State* l);
     int manager_behavior_getBoolean(lua_State* l);
