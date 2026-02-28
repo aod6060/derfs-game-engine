b4f757e4cf59f0355282f60500914126a1766a10
aod6060 <a.o.d.6060@gmail.com>
2026-02-28 08:26:16 -0600

    Documentation I was working on Last Night. I'll keep working on it.

--- bin/data/scripts/entity/trigger_test_entity.lua
+++ bin/data/scripts/entity/trigger_test_entity.lua
@@ -2,14 +2,18 @@
     This is an example lua script for the engine....
 ]]
 
-
+mesh = nil
 trigger = nil
 behavior = nil
+
+texture_enter = "snow"
+texture_exit = "angry"
 
 function init()
     -- This function is called once every
     behavior = manager_entity_getBehavior(entity)
     trigger = manager_component_trigger_getComponent(entity)
+    mesh = manager_component_mesh_getComponent(entity)
 
     manager_component_trigger_addEntityEnter(trigger, behavior, "player_enter")
     manager_component_trigger_addEntityExit(trigger, behavior, "player_exit")
@@ -29,8 +33,10 @@ end
 
 function player_enter()
     print("Player Enter")
+    manager_component_mesh_setTexture(mesh, texture_enter)
 end
 
 function player_exit()
     --print("Player Exit")
+    manager_component_mesh_setTexture(mesh, texture_exit)
 end
