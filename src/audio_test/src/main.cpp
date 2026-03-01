f0dc3d7493f8592f3eb739e44b9452d760b43905
aod6060 <a.o.d.6060@gmail.com>
2026-02-28 23:00:44 -0600

    #52 still struggling with learning about OpenAL and ogg/vorbis.

--- src/audio_test/src/main.cpp
+++ src/audio_test/src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <vorbis/codec.h>
+#include <vorbis/vorbisfile.h>
 
 
 int main(int argc, char** argv) {
-    std::cout << "Hello, World\n";
-
+    
     return 0;
 }
